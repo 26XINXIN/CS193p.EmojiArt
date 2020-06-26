92d6028a9af13c81ccfe4470913744aeef21d83a
Xin <yangxin.nlp@bytedance.com>
2020-06-26 11:32:53 -0500

    Initial Commit

---
+++ EmojiArt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by 杨鑫 on 2020/6/26.
+//  Copyright © 2020 杨鑫. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
